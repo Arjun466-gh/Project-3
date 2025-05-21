bef4d589424f54468450d6200afb863f35e315ed
Arjun <YOUR_EMAIL>
2025-05-21 14:31:37 -0700

    Project 3 Change

--- CryptoCalc 3/include/PriceLoader.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "FileLoader.hpp"
-#include "PricePoint.hpp"
-
-class PriceLoader : public FileLoader<PricePoint> {
-public:
-    std::vector<PricePoint> load(const std::string &filepath) const override;
-};
--- CryptoCalc 3/include/TransactionLoader.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "FileLoader.hpp"
-#include "Transaction.hpp"
-
-class TransactionLoader : public FileLoader<Transaction> {
-public:
-    std::vector<Transaction> load(const std::string &filepath) const override;
-};
